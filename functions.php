fb5c93715a85709d7622b3bb09c74d0e5af57aa6
rememb48 <corrado@debian>
2017-08-04 18:39:51 +0200

    nuova home

--- functions.php
+++ functions.php
@@ -1,4 +1,7 @@
 <?php
+@ini_set( 'upload_max_size' , '64M' );
+@ini_set( 'post_max_size', '64M');
+@ini_set( 'max_execution_time', '300' );
 /* Write your awesome functions below */
 add_action( 'wp_enqueue_scripts', 'zanca_enqueue_styles' );
 function zanca_enqueue_styles() {
