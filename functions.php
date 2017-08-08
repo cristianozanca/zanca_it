0163c6da10b6d66d5e938b4cf97ae6ce5ad3ccc6
rememb48 <corrado@debian>
2017-08-08 16:53:50 +0200

    page_ecomm template

--- functions.php
+++ functions.php
@@ -66,6 +66,19 @@ function load_dashicons_front_end() {
 	wp_enqueue_style( 'dashicons' );
 }
 
+/* page eCommerce two columns span  */
+
+
+add_filter( 'body_class', 'one_column_page_body_classes', 12 );
+
+function one_column_page_body_classes( $classes ) {
+	if ( is_page_template( 'page-templates/page_ecomm.php' ) && in_array('page-two-column', $classes) ) {
+		unset( $classes[array_search('page-two-column', $classes)] );
+		$classes[] = 'page-one-column';
+	}
+	return $classes;
+}
+
 
 
 
